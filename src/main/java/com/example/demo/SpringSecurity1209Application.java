488566b50fdf78dd475c3775ccffc5794e283098
大熊熊 <s86390474@yahoo.com>
2022-12-09 13:39:21 +0800

    eclipsests

---
+++ src/main/java/com/example/demo/SpringSecurity1209Application.java
@@ -0,0 +1,13 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringSecurity1209Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringSecurity1209Application.class, args);
+	}
+
+}
